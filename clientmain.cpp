bfcd4c46d07db91ab7ece0cab2d126e98497387f
patrik arlos <patrik.arlos@bth.se>
2020-09-08 18:31:14 +0200

    Initial commit after a mess up :)

---
+++ clientmain.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* You will to add includes here */
+
+
+// Included to get the support library
+#include <calcLib.h>
+
+int main(int argc, char *argv[]){
+  
+  /* Do magic */
+  
+
+}
---
+++ servermain.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* You will to add includes here */
+
+
+// Included to get the support library
+#include <calcLib.h>
+
+
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+  
+  /* Do more magic */
+  
+
+}
