4f57838cf8dedfb123be72e7d24c736555f78080
patrik arlos <patrik.arlos@bth.se>
2021-01-15 10:08:51 +0100

    Added input processing for client, it now accepts IP:port as argument.

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /* You will to add includes here */
 
@@ -7,8 +8,21 @@
 #include <calcLib.h>
 
 int main(int argc, char *argv[]){
-  
-  /* Do magic */
-  
 
+  /*
+    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
+     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
+  */
+  char delim[]=":";
+  char *Desthost=strtok(argv[1],delim);
+  char *Destport=strtok(NULL,delim);
+  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
+  // *Dstport points to whatever string came after the delimiter. 
+
+  /* Do magic */
+  int port=atoi(Destport);
+  printf("Host %s, and port %d.\n",Desthost,port);
+
+
+  
 }
